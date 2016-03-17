370ae6995d781e7b3493f848e320773c6e0190be
kid hualing <hualingson@foxmail.com>
2016-03-17 18:23:15 +0800

    migrate local repository

---
+++ src/_rbd.py
@@ -0,0 +1,17 @@
+#coding=utf-8
+'''
+Created on Feb 23, 2016
+
+@author: bob@huashanpai.net
+'''
+
+import rbd
+import rados
+
+if __name__ == '__main__':
+    ceph_conf = '/root/ceph/src/ceph.conf'
+    cluster = rados.Rados(conffile=ceph_conf)
+    cluster.connect(5)
+    from src import pp
+    pp.pprint(cluster.get_cluster_stats())
+    cluster.shutdown()
